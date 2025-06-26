72b0b97cbafcadaa926a182993a2642dec20ef22
Kyze <sagararizky1002@gmail.com>
2025-06-27 00:05:48 +0700

    up

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -2,163 +2,120 @@ import React from "react";
 import "./stars.scss";
 import imageOverlay from "../img/earth.jpg";
 
-class Contact extends React.Component {
-  render() {
-    return (
-      <section
-        className="footer-paralax bg-image sect-mt4 route" // hapus paralax-mf jika memberi efek biru
-        style={{
-          backgroundImage: "url(" + imageOverlay + ")",
-          backgroundColor: "transparent",
-        }} // pastikan backgroundColor transparan
-      >
-        <div id="stars" />
-        <div id="stars2" />
-        <div id="stars3" />
-        <div className="overlay-mf"></div>
-        <div className="container">
-          <div className="row">
-            <div className="col-sm-12">
-              <div className="contact-mf">
-                <div id="contact" className="box-shadow-full">
-                  <div className="row">
-                    <div className="col-md-6">
-                      <div className="title-box-2">
-                        <h5 className="title-left">Send A Message</h5>
-                      </div>
-                      <div>
-                        <form
-                          action="https://formspree.io/xdoeonlo"
-                          method="POST"
-                          className="contactForm"
-                        >
-                          <div id="sendmessage">
-                            Your message has been sent. Thank you!
-                          </div>
-                          <div id="errormessage"></div>
-                          <div className="row">
-                            <div className="col-md-12 mb-3">
-                              <div className="form-group">
-                                <input
-                                  type="text"
-                                  name="name"
-                                  className="form-control"
-                                  id="name"
-                                  placeholder="Your Name"
-                                  data-rule="minlen:4"
-                                  data-msg="Please enter at least 4 chars"
-                                />
-                                <div className="validation"></div>
-                              </div>
-                            </div>
-                            <div className="col-md-12 mb-3">
-                              <div className="form-group">
-                                <input
-                                  type="email"
-                                  className="form-control"
-                                  name="email"
-                                  id="email"
-                                  placeholder="Your Email"
-                                  data-rule="email"
-                                  data-msg="Please enter a valid email"
-                                />
-                                <div className="validation"></div>
-                              </div>
-                            </div>
-                            <div className="col-md-12 mb-3">
-                              <div className="form-group">
-                                <input
-                                  type="text"
-                                  className="form-control"
-                                  name="subject"
-                                  id="subject"
-                                  placeholder="Subject"
-                                  data-rule="minlen:4"
-                                  data-msg="Please enter at least 8 chars of subject"
-                                />
-                                <div className="validation"></div>
-                              </div>
-                            </div>
-                            <div className="col-md-12 mb-3">
-                              <div className="form-group">
-                                <textarea
-                                  className="form-control"
-                                  name="message"
-                                  rows="5"
-                                  data-rule="required"
-                                  data-msg="Please write something for us"
-                                  placeholder="Message"
-                                ></textarea>
-                                <div className="validation"></div>
-                              </div>
-                            </div>
-                            <div className="col-md-12">
-                              <button
-                                type="submit"
-                                className="button button-a button-big button-rouded"
-                              >
-                                Send Message
-                              </button>
-                            </div>
-                          </div>
-                        </form>
-                      </div>
+const socials = [
+  { icon: "ion-social-codepen", link: "https://codepen.io/" },
+  { icon: "ion-social-github", link: "https://github.com/SagaraKyzen" },
+  { icon: "ion-social-linkedin", link: "https://linkedin.com/in/yourprofile" },
+];
+
+const Contact = () => {
+  return (
+    <section
+      className="footer-paralax bg-image sect-mt4 route"
+      style={{
+        backgroundImage: `url(${imageOverlay})`,
+        backgroundColor: "transparent",
+      }}
+    >
+      <div id="stars" />
+      <div id="stars2" />
+      <div id="stars3" />
+      <div className="overlay-mf" />
+
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12">
+            <div className="contact-mf">
+              <div id="contact" className="box-shadow-full">
+                <div className="row">
+                  {/* Form (mailto) */}
+                  <div className="col-md-6">
+                    <div className="title-box-2">
+                      <h5 className="title-left">Send A Message</h5>
                     </div>
-                    <div className="col-md-6">
-                      <div className="title-box-2 pt-4 pt-md-0">
-                        <h5 className="title-left">Get in Touch</h5>
+                    <form
+                      action="mailto:sagararizky1002@gmail.com"
+                      method="POST"
+                      encType="text/plain"
+                    >
+                      <div className="row">
+                        <div className="col-md-12 mb-3">
+                          <input
+                            type="text"
+                            name="Name"
+                            className="form-control"
+                            placeholder="Your Name"
+                            required
+                          />
+                        </div>
+                        <div className="col-md-12 mb-3">
+                          <input
+                            type="email"
+                            name="Email"
+                            className="form-control"
+                            placeholder="Your Email"
+                            required
+                          />
+                        </div>
+                        <div className="col-md-12 mb-3">
+                          <input
+                            type="text"
+                            name="Subject"
+                            className="form-control"
+                            placeholder="Subject"
+                          />
+                        </div>
+                        <div className="col-md-12 mb-3">
+                          <textarea
+                            name="Message"
+                            rows="5"
+                            className="form-control"
+                            placeholder="Message"
+                            required
+                          ></textarea>
+                        </div>
+                        <div className="col-md-12">
+                          <button
+                            type="submit"
+                            className="button button-a button-big button-rouded"
+                          >
+                            Send via Email
+                          </button>
+                        </div>
                       </div>
-                      <div className="more-info">
-                        <p className="lead">
-                          Whether you want to get in touch, talk about a project
-                          collaboration, or just say hi, I'd love to hear from
-                          you.
-                          <br />
-                          Simply fill the from and send me an email.
-                        </p>
-                        {/* <!-- <ul class="list-ico">
-                                <li><span class="ion-ios-location"></span> 329 WASHINGTON ST BOSTON, MA 02108</li>
-                                <li><span class="ion-ios-telephone"></span> (617) 557-0089</li>
-                                <li><span class="ion-email"></span> contact@example.com</li>
-                                </ul> --> */}
-                      </div>
-                      <div className="socials">
-                        <ul>
-                          <li>
+                    </form>
+                  </div>
+
+                  {/* Info */}
+                  <div className="col-md-6">
+                    <div className="title-box-2 pt-4 pt-md-0">
+                      <h5 className="title-left">Get in Touch</h5>
+                    </div>
+                    <div className="more-info">
+                      <p className="lead">
+                        Whether you want to get in touch, collaborate, or just say hi — feel free!
+                        <br />
+                        This form will open your email client directly.
+                      </p>
+                    </div>
+
+                    {/* Social */}
+                    <div className="socials">
+                      <ul>
+                        {socials.map((s, i) => (
+                          <li key={i}>
                             <a
-                              href=""
+                              href={s.link}
                               target="_blank"
                               rel="noopener noreferrer"
                             >
                               <span className="ico-circle">
-                                <i className="ion-social-codepen"></i>
+                                <i className={s.icon}></i>
                               </span>
                             </a>
                           </li>
-                          <li>
-                            <a
-                              href=""
-                              target="_blank"
-                              rel="noopener noreferrer"
-                            >
-                              <span className="ico-circle">
-                                <i className="ion-social-github"></i>
-                              </span>
-                            </a>
-                          </li>
-                          <li>
-                            <a
-                              href=""
-                              target="_blank"
-                              rel="noopener noreferrer"
-                            >
-                              <span className="ico-circle">
-                                <i className="ion-social-linkedin"></i>
-                              </span>
-                            </a>
-                          </li>
-                        </ul>
-                      </div>
+                        ))}
+                      </ul>
                     </div>
                   </div>
                 </div>
@@ -166,18 +123,22 @@ class Contact extends React.Component {
             </div>
           </div>
         </div>
-        <footer>
-          <div className="container">
-            <div className="row">
-              <div className="col-sm-12">
-                <div className="copyright-box"></div>
+      </div>
+
+      {/* Footer */}
+      <footer>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="copyright-box text-center text-light">
+                &copy; {new Date().getFullYear()} Moechamad Rizky Sagara. All rights reserved.
               </div>
             </div>
           </div>
-        </footer>
-      </section>
-    );
-  }
-}
+        </div>
+      </footer>
+    </section>
+  );
+};
 
 export default Contact;
